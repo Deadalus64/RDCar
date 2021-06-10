dc46869e137cd4dd68ac0f2cff9c5e011853c301
Deadalus64 <kaan0serin@gmail.com>
2021-06-10 15:54:40 +0200

    Initial commit

---
+++ src/main/java/com/realdolmen/rdcar/RdCarApplication.java
@@ -0,0 +1,13 @@
+package com.realdolmen.rdcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdCarApplication.class, args);
+    }
+
+}
